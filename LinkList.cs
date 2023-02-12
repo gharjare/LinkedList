ccb9b15616d31ca93e6826fa0b4753b644d2f090
gharjare <gharjarechetan@gmail.com>
2023-02-12 18:16:24 +0530

    [chetan gharjare] create linked list

--- LinkList.cs
+++ LinkList.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    internal class LinkList
+    public class LinkList<T>
     {
+        public Node<T> head;
+        //Add Method For Add Value to Node
+        public void Add(T data)
+        {
+            //Creating object of node
+            Node<T> node = new Node<T>(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+        //Display Method For Displaying The node
+        public void Display()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
     }
 }
--- Node.cs
+++ Node.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    internal class Node
+    public class Node<T>
     {
+        public T data;
+        public Node<T> next;
+
+        public Node(T data)
+        {
+            this.data = data;
+        }
     }
 }
