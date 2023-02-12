38d46050651c56442f6767e7c9d9930f9c6983ea
gharjare <gharjarechetan@gmail.com>
2023-02-12 18:12:33 +0530

    [chetan gharjare] create linked list

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linkedlist program");
+            LinkList<int> linkedList = new LinkList<int>();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
         }
     }
 }
